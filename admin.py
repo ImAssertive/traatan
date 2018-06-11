aaa1c27754212af35d524129908197968fc05194
ImAssertive <imassertive@gmail.com>
2018-06-11 16:45:32 +0100

    w

--- admin.py
+++ admin.py
@@ -16,8 +16,8 @@ class adminCog:
     @commands.command(name="raidmode", aliases=['rm', 'raid', 'raidtoggle'])
     @checks.has_role("Admin", "Moderator")
     async def raidmode(self, ctx):
-        self.raidmodeState = not self.raidmodeState
-        await ctx.channel.send("Raid mode set to: " + self.raidmodeState)
+        self.raidmodeState = not str(self.raidmodeState)
+        await ctx.channel.send("Raid mode set to: " + str(self.raidmodeState))
 
     @commands.command(name="jail", aliases=['silence', 'unmute', 'unsilence', 'unjail'])
     @checks.has_role("Admin", "Moderator")
