46e3ab1e19dd51988eb75433356919ed815018c0
ImAssertive <imassertive@gmail.com>
2018-06-11 16:44:38 +0100

    w

--- admin.py
+++ admin.py
@@ -5,7 +5,7 @@ class adminCog:
     def __init__(self, bot):
         self.bot = bot
         self.welcomeChannel = 0
-        self.raidmode = False
+        self.raidmodeState = False
 
     @commands.command()
     @checks.has_role("Admin", "Moderator")
@@ -16,8 +16,8 @@ class adminCog:
     @commands.command(name="raidmode", aliases=['rm', 'raid', 'raidtoggle'])
     @checks.has_role("Admin", "Moderator")
     async def raidmode(self, ctx):
-        self.raidmode = not self.raidmode
-        await ctx.channel.send("Raid mode set to: " + self.raidmode)
+        self.raidmodeState = not self.raidmodeState
+        await ctx.channel.send("Raid mode set to: " + self.raidmodeState)
 
     @commands.command(name="jail", aliases=['silence', 'unmute', 'unsilence', 'unjail'])
     @checks.has_role("Admin", "Moderator")
@@ -25,14 +25,14 @@ class adminCog:
         memberid = useful.getid(member)
         if discord.utils.get(ctx.guild.roles, name= "jail") in ctx.guild.get_member(memberid).roles:
             await ctx.guild.get_member(memberid).remove_roles(discord.utils.get(ctx.guild.roles, name="jail"))
-            await ctx.channel.send("Unjailed " + ctx.guild.get_member(memberid).mention)
+            await ctx.channel.send("Unjailed " + ctx.guild.get_member(memberid).mention +"!")
 
         else:
             await ctx.guild.get_member(memberid).add_roles(discord.utils.get(ctx.guild.roles, name="jail"))
-            await ctx.channel.send("Jailed " + ctx.guild.get_member(memberid).mention)
+            await ctx.channel.send("Jailed " + ctx.guild.get_member(memberid).mention + "!")
 
     async def on_member_join(self, ctx):
-        if self.raidmode == True:
+        if self.raidmodeState == True:
             await ctx.add_roles(discord.utils.get(ctx.guild.roles, name="jail"))
             await ctx.send("This server is currently under attack! Please DM <@164921837107675137> or <@163691476788838401> if you are a legitimate user to get unjailed.")
         elif self.welcomeChannel != 0:
