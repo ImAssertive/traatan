c6170ea9d99aa70871204f917368bbd9301218ea
ImAssertive <imassertive@gmail.com>
2018-06-17 17:27:09 +0100

    w

--- tt2/admin.py
+++ tt2/admin.py
@@ -14,14 +14,16 @@ class adminCog:
     @checks.justme()
     async def botban(self, ctx, member):
         memberid = int(useful.getid(member))
-        self.bot.cur.execute("UPDATE Users SET banned=1 WHERE userID =?", memberid,)
+        print(memberid)
+        self.bot.cur.execute("UPDATE Users SET banned=1 WHERE userID =?", (memberid,))
         self.bot.con.commit()
 
     @commands.command()
     @checks.justme()
     async def botunban(self, ctx, member):
         memberid = int(useful.getid(member))
-        self.bot.cur.execute("UPDATE Users SET banned=0 WHERE userID =?", memberid,)
+        print(memberid)
+        self.bot.cur.execute("UPDATE Users SET banned=0 WHERE userID =?", memberid)
         self.bot.con.commit()
 
     async def on_guild_join(self, ctx):
