b5d2a104f0bbe4451f3b62864cc9865c44fd7538
ImAssertive <imassertive@gmail.com>
2018-06-19 14:29:31 +0100

    w

--- tt3/traatan.py
+++ tt3/traatan.py
@@ -2,32 +2,26 @@ import discord, asyncio, sys, traceback, checks, useful, asyncpg
 from discord.ext import commands
 
 
-# def getPrefix(bot, message):
-#     prefixes = ["tt!", "traa!"]
-#     return commands.when_mentioned_or(*prefixes)(bot, message)
+def getPrefix(bot, message):
+    prefixes = ["tt!", "traa!"]
+    return commands.when_mentioned_or(*prefixes)(bot, message)
 
-async def run():
-    initial_extensions = ['admin']
-    credFile = open("credentials.txt", "r")
-    credString = credFile.read()
-    credList = credString.split("\n")
-    credentials = eval(credList[1])
-    db = await asyncpg.create_pool(**credentials)
-    #await useful.createdb(db)
-    bot = commands.Bot(command_prefix="tt!", description='/r/Traa community help bot! tt!help for more info', db=db)
-    if __name__ == '__main__':
-        for extension in initial_extensions:
-            try:
-                bot.load_extension(extension)
-            except Exception as e:
-                print('Failed to load extension ' + extension, file=sys.stderr)
-                traceback.print_exc()
-    try:
-        bot.run(str(credList[0]))
-    except KeyboardInterrupt:
-        await db.close()
-        await bot.logout()
+initial_extensions = ['admin']
+credFile = open("credentials.txt", "r")
+credString = credFile.read()
+credList = credString.split("\n")
+credentials = eval(credList[1])
+db = await asyncpg.create_pool(**credentials)
+useful.createdb(db)
+bot = commands.Bot(command_prefix=getPrefix, description='/r/Traa community help bot! tt!help for more info')
+bot.db = db
 
+if __name__ == '__main__':
+    for extension in initial_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            print('Failed to load extension ' + extension, file=sys.stderr)
+            traceback.print_exc()
 
-asyncio.get_event_loop().run_until_complete(run())
-
+bot.run(str(credList[0]))
