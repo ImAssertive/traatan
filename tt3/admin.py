c83ac5e66a1b85528ddf190ec33f4856e557357f
ImAssertive <imassertive@gmail.com>
2018-06-21 10:48:05 +0100

    w

--- tt3/admin.py
+++ tt3/admin.py
@@ -8,9 +8,6 @@ class adminCog:
         self.bot = bot
 
 
-    @commands.command()
-    async def test(self, ctx):
-        await ctx.channel.send("nyoom desu poi")
     # @commands.command()
     # @checks.is_not_banned()
     # #@checks.has_roleedit_permission()
@@ -36,23 +33,21 @@ class adminCog:
     #     self.bot.con.commit()
     #
     async def on_guild_join(self, ctx):
-        connection = self.bot.db.acquire()
+        connection = await self.bot.db.acquire()
         async with connection.transaction():
             query = "INSERT INTO Guilds (guildID) VALUES($1) ON CONFLICT DO NOTHING"
             await self.bot.db.execute(query, ctx.id)
-        await bot.db.release(connection)
+        await self.bot.db.release(connection)
 
     async def on_member_join(self, ctx):
-        print(self.bot.db)
-        connection = self.bot.db.acquire()
-        print(connection)
+        connection = await self.bot.db.acquire()
         async with connection.transaction():
             query = "INSERT INTO Users (userID) VALUES($1) ON CONFLICT DO NOTHING"
             await self.bot.db.execute(query, ctx.id)
             query = "INSERT INTO GuildUsers (guildID, userID) VALUES($1, $2) ON CONFLICT DO NOTHING"
             await self.bot.db.execute(query, ctx.guild.id, ctx.id)
             print("nyoom desu poi")
-        await bot.db.release(connection)
+        await self.bot.db.release(connection)
 
 
 
