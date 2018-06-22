af67282962f6f73e0dce26b12fde0eca068e8004
ImAssertive <imassertive@gmail.com>
2018-06-22 10:15:14 +0100

    w

---
+++ tt3/misc.py
@@ -0,0 +1,61 @@
+import discord, asyncio, sys, traceback, checks, inflect, useful
+from discord.ext import commands
+
+class miscCog:
+    def __init__(self, bot):
+        self.bot = bot
+        self.welcomeChannel = 0
+        self.deleteBlueText = False
+
+    def blueTextFunction(self, userText):
+        blueText = ""
+        for counter in range(0,len(userText)):
+            if userText[counter].isalpha():
+                blueText += ":regional_indicator_" + userText[counter].lower() + ": "
+            elif userText[counter].isdigit():
+                blueText += ":" + inflect.engine().number_to_words(userText[counter]) + ":"
+            else:
+                blueText += userText[counter]
+        return blueText
+
+    @commands.command(name="bluetext", aliases=['bt'])
+    @checks.is_not_banned()
+    async def bluetext(self, ctx, *, userText):
+        toOutput = self.blueTextFunction(userText)
+        await ctx.channel.send(toOutput)
+        if self.deleteBlueText:
+            await ctx.message.delete()
+
+    @commands.command(name="bluetextcode", aliases=['bluetextmarkup', 'btc', 'btmu'])
+    @checks.is_not_banned()
+    async def bluetextcode(self, ctx, *, userText):
+        toOutput = self.blueTextFunction(userText)
+        await ctx.channel.send("Here's your code:")
+        await ctx.channel.send("```" + toOutput + "```")
+        if self.deleteBlueText:
+            await ctx.message.delete()
+
+    @commands.command()
+    @checks.is_not_banned()
+    async def cute(self, ctx, member):
+        memberID = useful.getid(member)
+        try:
+            toOutput = self.blueTextFunction(ctx.guild.get_member(memberID).nick +" is cute and valid and i love them")
+        except TypeError:
+            toOutput = self.blueTextFunction(ctx.guild.get_member(memberID).name +" is cute and valid and i love them")
+        await ctx.channel.send(toOutput)
+        if self.deleteBlueText:
+            await ctx.message.delete()
+
+    @commands.command(name="togglebluetextdelete", aliases=['deletebluetext', 'tbtd'])
+    @checks.justme()
+    async def toggleBlueTextDelete(self, ctx):
+        self.deleteBlueText = not self.deleteBlueText
+        if self.deleteBlueText:
+            await ctx.channel.send(":white_check_mark: Hiding bluetext commands!")
+        else:
+            await ctx.channel.send(":white_check_mark: No longer hiding bluetext commands!")
+
+
+def setup(bot):
+    bot.add_cog(miscCog(bot))
--- tt3/traatan.py
+++ tt3/traatan.py
@@ -75,7 +75,7 @@ async def run():
     accountPlatform text);''')
     bot = Bot(description=description, db=db)
     bot.test = "test"
-    initial_extensions = ['admin', 'setup']
+    initial_extensions = ['admin', 'setup', 'misc']
     if __name__ == '__main__':
         for extension in initial_extensions:
             try:
