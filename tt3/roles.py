02bbf23ab54467949adb5246960d357d53f692d0
ImAssertive <imassertive@gmail.com>
2018-06-27 16:31:00 +0100

    w

--- tt3/roles.py
+++ tt3/roles.py
@@ -5,6 +5,28 @@ class rolesCog:
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.group(pass_context=True)
+    async def roles(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.channel.send(":no_good: | Please enter a valid command. For a list of commands use: tt!roles help")
+
+    @roles.command()
+    async def add(self, ctx, *, roleName):
+        role = discord.utils.get(ctx.guild.roles, name=roleName)
+        print(role)
+        print(role.mention)
+
+    @roles.command()
+    @checks.is_not_banned()
+    async def help(self, ctx):
+        embed = discord.Embed(title="Roles Help Menu", description="", colour=self.bot.getcolour())
+        embed.add_field(name="Add", value="Adds a role to have customised bot permission levels.", inline=False)
+        embed.add_field(name="Remove/Delete", value="Removes a roles custom permission levels.", inline=False)
+        embed.add_field(name="Reset", value="Resets a roles permissions to default values.", inline=False)
+        embed.add_field(name="Select", value="Selects a role to edit custom permission levels for.", inline=False)
+        await ctx.channel.send(embed=embed)
+
+
 
 
 def setup(bot):
