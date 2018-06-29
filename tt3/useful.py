bc17be971efafdb98c2c32231fbdd9021cb38373
ImAssertive <imassertive@gmail.com>
2018-06-29 20:40:49 +0100

    w

--- tt3/useful.py
+++ tt3/useful.py
@@ -9,6 +9,6 @@ def getMenuEmoji(noOfOptions):
     emojis = [["one", "1\u20e3"],["two", "2\u20e3"],["three", "3\u20e3"],["four", "4\u20e3"], ["five", "5\u20e3"],["six", "6\u20e3"], ["seven", "7\u20e3"],["eight", "8\u20e3"],["nine", "9\u20e3"],["ten", "\U0001f51f"]]
     toReturn = []
     for i in range (0,noOfOptions):
-        toreturn.append(emojis[i])
+        toReturn.append(emojis[i])
     toReturn.append(["x", "❌"])
     return toReturn
