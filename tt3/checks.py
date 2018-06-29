3f741b7e29f473189409f12d860b752cc4d274eb
ImAssertive <imassertive@gmail.com>
2018-06-29 22:42:44 +0100

    w

--- tt3/checks.py
+++ tt3/checks.py
@@ -123,5 +123,5 @@ def roles_options2(ctx):
     return (ctx.content.lower() in ["enabled", "disabled", "enable", "disable", "true", "false", "info", "skip", "yes", "no", "close"]) and ctx.guild != None
 
 def roles_emojis1(ctx, reaction):
-    print(ctx.emoji)
+    print(reaction[1] == ctx.author)
     return str(ctx.emoji) in ["1\u20e3", "2\u20e3", "3\u20e3", "4\u20e3", "5\u20e3", "6\u20e3", "7\u20e3", "8\u20e3", "9\u20e3", "\U0001f51f"]
