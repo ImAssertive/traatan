37ea975bfe7719a9abe26d2d2c44d475c954956e
ImAssertive <imassertive@gmail.com>
2018-07-02 11:18:39 +0100

    fuck this

--- roles.py
+++ roles.py
@@ -87,9 +87,10 @@ class rolesCog:
                 for column in toeditTrue:
                     query = "UPDATE Roles SET " + column + " = true WHERE roleID = $1"
                     await self.bot.db.execute(query, role.id)
+                print(len(toeditTrue))
                 if len(toeditTrue) > 1:
                     toeditTrue = ', '.join(toeditTrue)
-                await ctx.channel.send(":white_check_mark: | The following permissions were granted to role "+role.name+": `" + toeditTrue + "`.")
+                await ctx.channel.send(":white_check_mark: | The following permissions were granted to role "+str(role.name)+": `" + toeditTrue + "`.")
 
             if toeditFalse != []:
                 for column in toeditFalse:
@@ -97,7 +98,7 @@ class rolesCog:
                     await self.bot.db.execute(query, role.id)
                 if len(toeditFalse) > 1:
                     toeditFalse = ', '.join(toeditFalse)
-                await ctx.channel.send(":white_check_mark: | The following permissions were revoked from role  "+role.name+": `" + toeditTrue + "`.")
+                await ctx.channel.send(":white_check_mark: | The following permissions were revoked from role  "+str(role.name)+": `" + toeditTrue + "`.")
 
         await self.bot.db.release(connection)
 
