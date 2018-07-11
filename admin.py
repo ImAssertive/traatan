8fd507a1121ba2b9f881c9563c15bb62287b28b1
ImAssertive <imassertive@gmail.com>
2018-07-11 22:40:28 +0100

    wew

--- admin.py
+++ admin.py
@@ -419,7 +419,17 @@ class adminCog:
     @commands.command(name = "exit", aliases =['quit'], hidden = True)
     @checks.justme()
     async def exit(self, ctx):
-        await ctx.channel.send(":wave: Goodbye.")
+        thanos = random.randint(1,5)
+        if thanos == 1:
+            await ctx.channel.send("Mrs Assertive I dont feel so good...")
+        if thanos == 2:
+            await ctx.channel.send("Why...")
+        if thanos == 3:
+            await ctx.channel.send(":wave: Goodbye.")
+        if thanos == 4:
+            await ctx.channel.send("Faster, Bambi! Don't look back! Keep running! Keep running!")
+        if thanos == 5:
+            await ctx.channel.send("The horror. The horror.")
         await self.bot.db.close()
         await self.bot.logout()
         sys.exit()
