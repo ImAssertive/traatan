830bae46ece85df0a59e2a62ba817a36f9f02485
ImAssertive <imassertive@gmail.com>
2018-07-18 17:42:31 +0100

    wew

--- nsfw.py
+++ nsfw.py
@@ -17,7 +17,7 @@ class nsfwCog:
             imageembed = discord.Embed(description="Tags: "+keywordsText,colour=self.bot.getcolour())
             imagetopost = random.randint(0, len(results))
             imageembed.set_image(url=results[imagetopost].file_url)
-            imageembed.set_footer(text="Image "+imagetopost+" on e621 of "+str(len(results))+" results.")
+            imageembed.set_footer(text="Image "+str(imagetopost)+" on e621 of "+str(len(results))+" results.")
             await ctx.channel.send(embed=imageembed)
         else:
            await ctx.channel.send(':no_entry: | No results found!')
