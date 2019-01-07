6bf1134d65f4396fefdfcee84227fd3ffd6316d8
ImAssertive <imassertive@gmail.com>
2019-01-07 23:29:46 +0000

    added dependancies

--- admin.py
+++ admin.py
@@ -1,4 +1,4 @@
-import discord, asyncio, sys, traceback, checks, useful, asyncpg, random
+import discord, asyncio, sys, traceback, checks, useful, asyncpg, random, ast
 from discord.ext import commands
 
 
